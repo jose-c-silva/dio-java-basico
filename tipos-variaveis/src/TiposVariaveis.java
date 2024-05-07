f75c8b4be99433d6c1512580f66db904f02f9423
José Antônio Silva <22.jr.silva@gmail.com>
2024-05-07 16:12:04 -0300

    utilizadas todas as variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,6 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500.33;
+        System.out.println(salarioMinimo);
     }
 }
