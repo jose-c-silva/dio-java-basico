63d4041e28078b1dc4c43d47af441e54e1c11f81
José Antônio Silva <22.jr.silva@gmail.com>
2024-05-09 15:13:05 -0300

    adicionar plano operadora e sistema medida

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,23 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "G";
+
+        switch (sigla) {
+            case "P":{
+                System.out.println("Pequeno");
+                break;
+            }
+            case "M":{
+                System.out.println("Médio");
+                break;
+            }
+            case "G":{
+                System.out.println("Grande");
+                break;
+            }
+            default:{
+                System.out.println("Sua letra não se aplica");
+            }
+        }
+    }
+}
